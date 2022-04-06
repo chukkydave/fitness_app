47b98af359eb701892da766412969d6f867c2fee
chukkydave <chukkydave@gmail.com>
2022-04-06 13:34:40 +0100

    finish setup and installation of material ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import Typography from '@mui/material/Typography';
+import Textfield from '@mui/material/TextField';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [
+		exercises,
+		setExercises,
+	] = useState({ exercises: [], title: '' });
+
+	return (
+		<div>
+			<Typography variant="header1" align="center" gutterBottom>
+				Exercise
+			</Typography>
+		</div>
+	);
 }
-
-export default App;
